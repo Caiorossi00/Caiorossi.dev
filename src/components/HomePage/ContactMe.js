fbbe7efc45a4957645e77dcfb1fd75b75247f661
Caiorossi00 <caio_rossi56@hotmail.com>
2025-02-22 15:27:30 -0300

    Add ContactMe component and integrate it into HomePage layout; create ContactMe.scss for styling

---
+++ src/components/HomePage/ContactMe.js
@@ -0,0 +1,32 @@
+import React from "react";
+import "../../assets/scss/ContactMe.scss";
+
+const ContactMe = () => {
+  return (
+    <div className="contactMe">
+      <h1>Entre em contato</h1>
+      <div className="contactMeBtns">
+        <a
+          href="https://www.instagram.com/caiorossi.dev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="CM-instagram"
+        >
+          <p>Instagram</p>
+          <i className="fa-brands fa-instagram"></i>
+        </a>
+        <a
+          href="https://wa.me/5553984158694"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="CM-whatsapp"
+        >
+          <p>Whatsapp</p>
+          <i className="fa-brands fa-whatsapp"></i>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default ContactMe;
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -3,6 +3,7 @@ import Intro from "../components/HomePage/Intro";
 import LatestProjects from "../components/HomePage/LatestProjects";
 import "../assets/scss/HomePage.scss";
 import ClientsFeedback from "../components/HomePage/ClientsFeedback";
+import ContactMe from "../components/HomePage/ContactMe";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
       <Intro />
       <LatestProjects />
       <ClientsFeedback />
+      <ContactMe />
     </div>
   );
 };
