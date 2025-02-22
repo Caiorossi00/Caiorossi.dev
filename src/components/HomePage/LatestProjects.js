e96e243e3e9a8a8bc24b02e848f34e619c49f6d6
Caiorossi00 <caio_rossi56@hotmail.com>
2025-02-22 11:42:29 -0300

    Refactor LatestProjects component: move to a new file and update imports in HomePage

--- src/components/HomePage/LatestProjects.js
+++ src/components/HomePage/LatestProjects.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../../assets/scss/LatestProjects.scss";
+import "../../assets/scss/LatestProjects.scss";
 
 const LatestProjects = () => {
   return (
--- src/routes/HomePage.js
+++ src/routes/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Intro from "../components/HomePage/Intro";
+import LatestProjects from "../components/HomePage/LatestProjects";
 import "../assets/scss/HomePage.scss";
-import LatestProjects from "../components/HomePage/LatestProjects/LatestProjects";
 
 const HomePage = () => {
   return (
