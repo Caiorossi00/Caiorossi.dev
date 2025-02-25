619d13ca2e1d767fc947a9e5b6519932fbad65ae
Caiorossi00 <caio_rossi56@hotmail.com>
2025-02-25 19:43:12 -0300

    Implement Experiences section with data mapping and styling; add Experiences.scss for layout

---
+++ src/assets/data/experiences.js
@@ -0,0 +1,53 @@
+const experiences = [
+  {
+    id: 1,
+    title: "Caiorossi.dev",
+    image: "https://avatars.githubusercontent.com/u/142348012?v=4",
+    role: "Freelancer",
+    description:
+      "Curso voltado para desenvolvimento de software e arquitetura de sistemas.",
+    period: "set/2023 - Atualmente",
+  },
+  {
+    id: 2,
+    title: "Fronteira Agro",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTb8x3cLgDrd6CfdLYJq6MVoJymKV1BzPrvUw&s",
+    role: "",
+    description:
+      "Curso voltado para desenvolvimento de software e arquitetura de sistemas.",
+    period: "set/2024 - Atualmente",
+  },
+  {
+    id: 3,
+    title: "Fronteira Agro",
+    image:
+      "https://ugc.production.linktr.ee/8aff584d-8189-4559-bc9c-29e1977f641e_infinitysports-marca-cmyk.jpeg?io=true&size=avatar-v3_0",
+    role: "",
+    description:
+      "Curso voltado para desenvolvimento de software e arquitetura de sistemas.",
+    period: "abr/2024 - Atualmente",
+  },
+  {
+    id: 4,
+    title: "Professor particular de Inglês",
+    image:
+      "https://t3.ftcdn.net/jpg/01/87/20/32/360_F_187203256_Li8bjvTWRGjEPM50xhHAPGU40fBUrK92.jpg",
+    role: "",
+    description:
+      "Curso voltado para desenvolvimento de software e arquitetura de sistemas.",
+    period: "jun/2022 - jul/2023",
+  },
+  {
+    id: 5,
+    title: "Secretaria de Cultura e Turismo de Jaguarão",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHD5EKatYDFxP6WhvRs6UrzkWOj4ozLu8Qzg&s",
+    role: "Estagiário",
+    description:
+      "Curso voltado para desenvolvimento de software e arquitetura de sistemas.",
+    period: "abr/2018 - out/2019",
+  },
+];
+
+export default experiences;
--- src/components/AboutMe/Experiences.js
+++ src/components/AboutMe/Experiences.js
@@ -1,7 +1,28 @@
-import React from "react";
+import experiences from "../../assets/data/experiences";
+import "../../assets/scss/Experiences.scss";
 
-const Experiences = () => {
-  return <div>Experiences</div>;
+const Experience = () => {
+  return (
+    <div className="experience-container">
+      <h2>Experiências</h2>
+
+      <div className="experience-content">
+        {experiences.map((exp) => (
+          <div key={exp.id} className="experience-item">
+            <img src={exp.image} alt={exp.title} className="experience-image" />
+            <div>
+              <div className="experience-title-and-period">
+                <h3>{exp.title}</h3>
+                <small>{exp.period}</small>
+              </div>
+              <p className="role">{exp.role}</p>
+              <p className="description">{exp.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
-export default Experiences;
+export default Experience;
