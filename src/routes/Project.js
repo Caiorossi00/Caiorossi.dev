f2dba366c22e365cadb206b77acdc70b4cac861d
Caiorossi00 <caio_rossi56@hotmail.com>
2025-03-25 16:00:15 -0300

    Refactor Project component to import projectData from a separate file; update ProjectNavbar styling for improved layout

--- src/routes/Project.js
+++ src/routes/Project.js
@@ -1,35 +1,7 @@
 import React, { useState } from "react";
 import "../assets/scss/Project.scss";
 import ProjectNavbar from "../components/Project/ProjectNavbar";
-
-const projectData = {
-  name: "Nome Projeto",
-  description:
-    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...",
-  technicalDetails: [
-    { label: "Client", value: "Caio Rossi" },
-    { label: "Ano", value: "2024" },
-    { label: "Categoria", value: "Landing Page" },
-    {
-      label: "Tecnologias",
-      value: [
-        { name: "React", icon: "devicon-react-original colored" },
-        { name: "Sass", icon: "devicon-sass-original colored" },
-        { name: "PostgreSQL", icon: "devicon-postgresql-plain colored" },
-      ],
-    },
-  ],
-  starDetails: [
-    {
-      title: "Situação",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-    },
-  ],
-  images: [
-    "https://images.unsplash.com/photo-1554668048-5055c5654bbc?q=80&w=2070&auto=format&fit=crop",
-    "https://images.unsplash.com/photo-1554668048-5055c5654bbc?q=80&w=2070&auto=format&fit=crop",
-  ],
-};
+import projectData from ".././assets/data/projectData";
 
 const Project = ({ setActivePage }) => {
   const [isStarExpanded, setIsStarExpanded] = useState(false);
