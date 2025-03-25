26708c967c8fe71377e6a5adc139d0f73f895b06
Caiorossi00 <caio_rossi56@hotmail.com>
2025-03-25 08:37:33 -0300

    Add DetailsAboutMe component and integrate it into AboutMe; enhance styling for improved presentation

---
+++ src/components/AboutMe/DetailsAboutMe.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../assets/scss/detailsAboutMe.scss";
+
+const DetailsAboutMe = () => {
+  return (
+    <div id="DetailsAboutMe">
+      <h2>Sobre Mim</h2>
+      <p>
+        Tenho 24 anos, atuo como desenvolvedor front-end freelancer. Busco
+        encontrar soluções funcionais e criativas utilizando código para
+        transportar ideias para o mundo real. O estudo da programação é uma
+        constante diária há quase um ano e meio. Concluí alguns dos principais
+        cursos do mundo na área, como o CS50x e o CS50T, de Harvard, e hoje
+        curso a faculdade de Análise e Desenvolvimento de Dados na faculdade
+        Descomplica. Fora da área de tecnologia, tenho formação em Turismo pela
+        faculdade UNIPAMPA.
+      </p>
+    </div>
+  );
+};
+
+export default DetailsAboutMe;
--- src/routes/AboutMe.js
+++ src/routes/AboutMe.js
@@ -5,11 +5,13 @@ import Experiences from "../components/AboutMe/Experiences";
 import ContactMe from "../components/ContactMe";
 import Footer from "../components/Footer";
 import "../assets/scss/AboutMe.scss";
+import DetailsAboutMe from "../components/AboutMe/DetailsAboutMe";
 
 const AboutMe = () => {
   return (
     <div id="AboutMe">
       <Intro />
+      <DetailsAboutMe />
       <Education />
       <Experiences />
       <ContactMe />
